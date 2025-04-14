cfed7c5436c6cafb6a9eef904ae4dde8e69db10e
Harsh Narayan Jha <harshnj@proton.me>
2025-04-14 22:18:41 +0530

    fix tailwind@v4 setup

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ mod components;
 mod models;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
@@ -16,9 +15,8 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
 
-        Home {}
+        div { id: "root", class: "w-full min-h-svh p-8", Home {} }
     }
 }
