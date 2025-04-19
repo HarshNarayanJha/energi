000c9163b117af56add973e0667dd5310f704a4c
Harsh Narayan Jha <harshnj@proton.me>
2025-04-19 21:45:04 +0530

    tauri: add upower_dbus and zbus and export get battery data function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,18 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+mod upower;
+
+use upower::get_battery_data;
+use upower::BatteryData;
+
+#[tauri::command(async)]
+async fn battery_data() -> Result<BatteryData, String> {
+    return get_battery_data().await.map_err(|err| err.to_string());
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![battery_data])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
