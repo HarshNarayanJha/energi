6e92707a046aae2d410303ded8f12071f51684be
Harsh Narayan Jha <harshnj@proton.me>
2025-04-19 15:55:46 +0530

    setup empty tauri project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+function App() {
+	return (
+		<main className="container">
+			<h1 className="text-2xl">Energi</h1>
+		</main>
+	)
+}
+
+export default App
--- src/models/mod.rs
+++
@@ -1,5 +0,0 @@
-mod battery_data;
-mod upower;
-
-pub use battery_data::BatteryData;
-pub use upower::get_battery_data;
